d94dcff01ee27bd8e503b7526b8ed569ddf8e7ee
Danny Ayoub <Danny Ayoub>
2016-05-11 21:54:03 -0400

    Semi-final version

---
+++ corrimg.m
@@ -0,0 +1,12 @@
+function [ out ] = corrimg( in, f )
+%This function manually filters an image using the 2-D convolution
+%   Implemented in fulfillment of the term project for ECE 346 on the topic
+%   of image processing.
+%   By Alexi Varughese, Wayne Sun, and Daniel Ayoub
+    in = double(in);
+    out(:,:,1) = xcorr2(in(:,:,1), f);
+    out(:,:,2) = xcorr2(in(:,:,2), f);
+    out(:,:,3) = xcorr2(in(:,:,3), f);
+    out = uint8(out);
+end
+
--- main.m
+++ main.m
@@ -7,7 +7,33 @@
 
 img = imread('guineapig.jpg');
 
-% 2-D Low-pass filter
+% Channels convolved using conv2 function - 10px blur
+figure;
+f = 1/100 .* ones(10,10);
+img_lp10 = convimg(img, f);
+image(img_lp10); axis equal tight;
+set(gca,'xtick',[]); set(gca,'xticklabel',[]);
+set(gca,'ytick',[]); set(gca,'yticklabel',[]);
+title('2-D convolution with 10x10 Moving Average Filter');
+
+figure;
+subplot(1, 2, 1);
+h = [1 1 1; 0 0 0; -1 -1 -1];
+img_edgeconv = convimg(img, h);
+img_edgecorr = corrimg(img, h);
+% Display convolved image
+image(img_edgeconv); axis equal tight;
+set(gca,'xtick',[]); set(gca,'xticklabel',[]);
+set(gca,'ytick',[]); set(gca,'yticklabel',[]);
+title('2-D Edge detect filter convolved with image');
+subplot(1, 2, 2);
+% Display correlated image
+image(img_edgecorr); axis equal tight;
+set(gca,'xtick',[]); set(gca,'xticklabel',[]);
+set(gca,'ytick',[]); set(gca,'yticklabel',[]);
+title('2-D Edge detect filter correlated with image');
+
+%% 2-D Low-pass filter
 
 figure;
 title('blur'); 
@@ -61,6 +87,65 @@ image(img_lp1x50); axis equal tight;
 set(gca,'xtick',[]); set(gca,'xticklabel',[]);
 set(gca,'ytick',[]); set(gca,'yticklabel',[]);
 title('1x50px Blur');
+%% Filter Examples
 
-%% Gaussian Blur
-f = @(m, n, sig) 1000/(2*pi*sig^2) * exp(-1*(m.^2+n.^2)/(2*sig^2)); 
+emboss = [-2 1 0; -1 1 1; 0 1 2];
+edgedetect = [0 1 0;1 -4 1;0 1 0];
+edgeenhance = [0 0 0; -1 1 0; 0 0 0];
+sharpen = [0 0 0 0 0; 0 0 -1 0 0; 0 -1 5 -1 0; 0 0 -1 0 0; 0 0 0 0 0];
+
+Hem = imfilter(img,emboss,'replicate');
+Hed = imfilter(img,edgedetect,'replicate');
+Hedg = imfilter(img,edgeenhance,'replicate');
+Hsharp = imfilter(img, sharpen, 'replicate');
+
+figure
+subplot(2,2,1)
+image(Hem)
+axis equal tight;
+set(gca,'xtick',[]); set(gca,'xticklabel',[]);
+set(gca,'ytick',[]); set(gca,'yticklabel',[]);
+title('Embossing');
+
+subplot(2,2,2)
+image(Hed)
+axis equal tight;
+set(gca,'xtick',[]); set(gca,'xticklabel',[]);
+set(gca,'ytick',[]); set(gca,'yticklabel',[]);
+title('Edge Detection');
+
+subplot(2,2,3)
+image(Hedg+img)
+axis equal tight;
+set(gca,'xtick',[]); set(gca,'xticklabel',[]);
+set(gca,'ytick',[]); set(gca,'yticklabel',[]);
+title('Edge Enhancement');
+
+subplot(2,2,4)
+image(Hsharp)
+axis equal tight;
+set(gca,'xtick',[]); set(gca,'xticklabel',[]);
+set(gca,'ytick',[]); set(gca,'yticklabel',[]);
+title('Sharpening');
+
+%% Unsharp masking
+img = imread('eye.jpg');
+unsharp = [0 -1 0; -1 4 -1; 0 -1 0];
+sharp = [0 -1 0; -1 5 -1; 0 -1 0];
+subplot(2, 2, 1);
+imshow(img);
+title('Original Image')
+subplot(2, 2, 2);
+img_sharpened = corrimg(img, sharp);
+imshow(img_sharpened);
+title('Sharpening Filter');
+subplot(2, 2, 3);
+imshow(corrimg(img, unsharp));
+title('Unsharp Mask');
+img_mask = corrimg(img, unsharp);
+unsharp = [0 0 0; 0 1 0; 0 0 0] - fspecial('gaussian');
+subplot(2, 2, 4);
+img_mask2 = img_mask(2:184, 2:251, 1:3);
+img_unsharp = img + img_mask2;
+imshow(img_unsharp);
+title('Unsharp mask + original image');
